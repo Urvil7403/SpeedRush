1495463cf2b6e2237e2ee4ced264040324633eb8
Urvil7403 <138550030+Urvil7403@users.noreply.github.com>
2024-09-29 15:07:07 -0400

    Publishing my game on github

---
+++ RacingGame/RacingGame/AboutScene.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RacingGame
+{
+    public class AboutScene : GameScene
+    {
+        private SpriteBatch spriteBatch;
+
+        private Rectangle rectangle;
+        private string aboutText = "Created by : \n\nMalav Brahmbhatt - 8833650 \nShrey Patel - 8865805 \nUrvil Shah - 8845239 \nSneh Patel - 886860";
+
+        Vector2 textSize;
+        Vector2 textPosition;
+
+        private SpriteFont font, titleFont;
+
+        public AboutScene(Game game) : base(game)
+        {
+            Game1 g = (Game1)game;
+            this.spriteBatch = g._spriteBatch;
+
+            font = g.Content.Load<SpriteFont>("RegularFont");
+            titleFont = g.Content.Load<SpriteFont>("TitleFont");
+
+            textSize = font.MeasureString(aboutText);
+
+            rectangle = new Rectangle(0, 0, g.GraphicsDevice.Viewport.Width, g.GraphicsDevice.Viewport.Height);
+        }
+
+
+        public override void Draw(GameTime gameTime)
+        {
+            spriteBatch.Begin();
+
+            textPosition = new Vector2(rectangle.Center.X - textSize.X / 2, rectangle.Center.Y - textSize.Y);
+
+            spriteBatch.DrawString(titleFont, "About", new Vector2(rectangle.Center.X - titleFont.MeasureString("About").X / 2, rectangle.Center.Y - textSize.Y - 50), Color.White);
+
+            spriteBatch.DrawString(font, aboutText, textPosition, Color.White);
+
+
+
+            spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ RacingGame/RacingGame/Truck.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace RacingGame
+{
+    public class Truck : DrawableGameComponent
+    {
+        private SpriteBatch spriteBatch;
+        private Texture2D carTexture;
+        private Vector2 position;
+        private float speed = 3f;
+
+
+        public Vector2 Position { get => position; }
+        public bool IsOffScreen { get; private set; }
+
+        public float Speed { get => speed; set => speed = value; }
+
+
+        Vector2 scale = new Vector2(0.8f, 0.8f);
+
+        public Rectangle BoundingBox
+        {
+            get
+            {
+                var centerX = position.X + carTexture.Width * scale.X /6;
+                return new Rectangle((int)centerX, (int)position.Y, (int)(carTexture.Width * scale.X * 0.75), (int)(carTexture.Height * scale.Y));
+            }
+        }
+
+
+        public Truck(Game game, SpriteBatch spriteBatch, Vector2 position) : base(game)
+        {
+            this.spriteBatch = spriteBatch;
+
+            this.position = position;
+            carTexture = game.Content.Load<Texture2D>("truck");
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            position.Y += speed;
+
+            if (position.Y > Game.Window.ClientBounds.Height)
+            {
+                IsOffScreen = true;
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(carTexture, position, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+
+            spriteBatch.End();
+        }
+    }
+
+}
